3e894baeb20450d6c1b04ff6bd925f1103403008
Dat Nguyen <vandatnguyen1896@gmail.com>
2019-08-09 14:04:15 +0700

    Add handle custom error when go wrong controller

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using CentralizedDataSystem.Resources;
+using System.Web.Mvc;
+
+namespace CentralizedDataSystem.Controllers {
+    public class ErrorController : Controller {
+        
+        public ActionResult Index() {
+            return View(ViewName.ERROR_UNKNOWN);
+        }
+
+        public ViewResult NotFound() {
+            Response.StatusCode = 404;
+
+            return View(ViewName.ERROR_404);
+        }
+    }
+}

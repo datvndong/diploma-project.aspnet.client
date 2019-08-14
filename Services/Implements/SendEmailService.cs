d61863873fc8c8e4cc4a2352ef6bc4434c81ec61
Dat Nguyen <vandatnguyen1896@gmail.com>
2019-08-14 21:05:42 +0700

    Change pattern to Singleton for send email feature

--- Services/Implements/SendEmailService.cs
+++ Services/Implements/SendEmailService.cs
@@ -20,43 +20,6 @@ namespace CentralizedDataSystem.Services.Implements {
             _userService = userService;
         }
 
-        private bool ProcessSend(string targetEmail, string subject, string content) {
-            try {
-                string host = ConfigUtil.GetKeyConfig("SMTPHost");
-                int port = int.Parse(ConfigUtil.GetKeyConfig("SMTPPort"));
-                string fromEmail = ConfigUtil.GetKeyConfig("FromEmailAddress");
-                string password = ConfigUtil.GetKeyConfig("FromEmailPassword");
-                string fromName = ConfigUtil.GetKeyConfig("FromName");
-
-                SmtpClient smtpClient = new SmtpClient(host, port) {
-                    // No using defaut
-                    UseDefaultCredentials = false,
-                    // Create a new credential
-                    Credentials = new System.Net.NetworkCredential(fromEmail, password),
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    EnableSsl = true,
-                    Timeout = 100000
-                };
-
-                MailMessage mail = new MailMessage {
-                    Body = content,
-                    Subject = subject,
-                    From = new MailAddress(fromEmail, fromName)
-                };
-
-                mail.To.Add(new MailAddress(targetEmail));
-                mail.BodyEncoding = System.Text.Encoding.UTF8;
-                mail.IsBodyHtml = true;
-                mail.Priority = MailPriority.High;
-
-                smtpClient.Send(mail);
-
-                return true;
-            } catch (Exception) {
-                return false;
-            }
-        }
-
         private List<User> CreateListUserFromJson(List<User> users, string content) {
             JArray jArray = JArray.Parse(content);
             JObject dataObject = null;
@@ -103,6 +66,9 @@ namespace CentralizedDataSystem.Services.Implements {
             content = content.Replace("{{title}}", Messages.MAIL_TITLE);
             content = content.Replace("{{link}}", domainName);
 
+            // Add mail info
+            EmailUtil.Instance.AddMailInfo(subject, content);
+
             if (assign.Equals(Keywords.AUTHENTICATED)) {
                 string res = await _userService.FindAllUsers(token);
 
@@ -112,12 +78,13 @@ namespace CentralizedDataSystem.Services.Implements {
             }
 
             bool sendResult = false;
-            string email = string.Empty;
+            string emailAddress = string.Empty;
             foreach (User user in users) {
-                email = user.Email;
-                sendResult = ProcessSend(email, subject, content);
+                emailAddress = user.Email;
+                sendResult = EmailUtil.Instance.ProcessSend(emailAddress);
+
                 if (!sendResult) {
-                    errorMails.Add(email);
+                    errorMails.Add(emailAddress);
                 }
             }
 
---
+++ Utils/EmailUtil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace CentralizedDataSystem.Utils {
+    public sealed class EmailUtil {
+        public static EmailUtil Instance { get; } = new EmailUtil();
+        private static SmtpClient smtpClient;
+        private static MailMessage mailMessage;
+
+        static EmailUtil() { }
+
+        private EmailUtil() {
+            string host = ConfigUtil.GetKeyConfig("SMTPHost");
+            int port = int.Parse(ConfigUtil.GetKeyConfig("SMTPPort"));
+            string fromEmail = ConfigUtil.GetKeyConfig("FromEmailAddress");
+            string password = ConfigUtil.GetKeyConfig("FromEmailPassword");
+            string fromName = ConfigUtil.GetKeyConfig("FromName");
+
+            smtpClient = new SmtpClient(host, port) {
+                // No using defaut
+                UseDefaultCredentials = false,
+                // Create a new credential
+                Credentials = new System.Net.NetworkCredential(fromEmail, password),
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                EnableSsl = true,
+                Timeout = 100000
+            };
+
+            mailMessage = new MailMessage {
+                From = new MailAddress(fromEmail, fromName),
+                BodyEncoding = System.Text.Encoding.UTF8,
+                IsBodyHtml = true,
+                Priority = MailPriority.High
+            };
+        }
+
+        public void AddMailInfo(string subject, string content) {
+            mailMessage.Subject = subject;
+            mailMessage.Body = content;
+        }
+
+        public bool ProcessSend(string address) {
+            try {
+                mailMessage.To.Clear();
+                mailMessage.To.Add(address);
+
+                smtpClient.Send(mailMessage);
+                return true;
+            } catch (Exception) {
+                return false;
+            }
+        }
+    }
+}
